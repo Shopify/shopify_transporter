3525f47a438d49f08ad3b70ecd1e9cb266a6318e
Jordan Verasamy <j.verasamy@gmail.com>
2018-07-12 12:07:05 -0400

    add expectations for amount of calls

--- spec/sample_scripts/third_party_platform_exporting/magento/v1.9/soap/orders_spec.rb
+++ spec/sample_scripts/third_party_platform_exporting/magento/v1.9/soap/orders_spec.rb
@@ -13,16 +13,16 @@ RSpec.describe OrderExporter do
       sales_order_list_response_body = double('sales_order_list_response_body')
       sales_order_info_response_body = double('sales_order_info_response_body')
 
-      expect(soap_client).to receive(:call).with(:login, anything).and_return(login_response_body)
+      expect(soap_client).to receive(:call).with(:login, anything).and_return(login_response_body).exactly(:once)
       expect(login_response_body).to receive(:body).and_return(
         {
           login_response: {
             login_return: 12345
           }
         }
-      )
+      ).exactly(:once)
 
-      expect(soap_client).to receive(:call).with(:sales_order_list, anything).and_return(sales_order_list_response_body)
+      expect(soap_client).to receive(:call).with(:sales_order_list, anything).and_return(sales_order_list_response_body).at_least(:once)
       expect(sales_order_list_response_body).to receive(:body).and_return(
         {
           sales_order_list_response: {
@@ -33,16 +33,16 @@ RSpec.describe OrderExporter do
             }
           }
         }
-      )
+      ).at_least(:once)
 
-      expect(soap_client).to receive(:call).with(:sales_order_info, anything).and_return(sales_order_info_response_body)
+      expect(soap_client).to receive(:call).with(:sales_order_info, anything).and_return(sales_order_info_response_body).at_least(:once)
       expect(sales_order_info_response_body).to receive(:body).and_return(
         {
           sales_order_info_response: {
             some_detail: "yep for sure"
           }
         }
-      )
+      ).at_least(:once)
 
       allow_any_instance_of(TransporterExporter).to receive(:soap_client).and_return(soap_client)
 

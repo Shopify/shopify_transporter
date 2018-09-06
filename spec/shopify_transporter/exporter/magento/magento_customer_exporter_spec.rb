29a50cc00ed6bdc14c29c1042f7a00b5392ac248
Jordan Verasamy <j.verasamy@gmail.com>
2018-09-06 13:43:17 -0400

    move tests to their own files, add test for exporter factory

---
+++ spec/shopify_transporter/exporter/magento/magento_customer_exporter_spec.rb
@@ -0,0 +1,49 @@
+# frozen_string_literal: true
+require 'shopify_transporter/pipeline/stage'
+
+RSpec.describe ShopifyTransporter::MagentoCustomerExporter do
+  context '#run' do
+    it 'works' do
+      soap_client = double("soap client")
+      customer_customer_list_response_body = double('customer_customer_list_response_body')
+      customer_address_list_response_body = double('customer_address_list_response_body')
+      expect(soap_client).to receive(:call).with(:customer_customer_list, anything).and_return(customer_customer_list_response_body).at_least(:once)
+      expect(customer_customer_list_response_body).to receive(:body).and_return(
+        {
+          customer_customer_list_response: {
+            store_view: {
+              item: [
+                {
+                  customer_id: 654321,
+                  top_level_attribute: "an_attribute"
+                }
+              ]
+            }
+          }
+        }
+      ).at_least(:once)
+      expect(soap_client).to receive(:call).with(:customer_address_list, customer_id: 654321).and_return(customer_address_list_response_body).at_least(:once)
+      expect(customer_address_list_response_body).to receive(:body).and_return(
+        {
+          customer_address_list_response: {
+            result: {
+              customer_address_attribute: "another_attribute"
+            }
+          }
+        }
+      ).at_least(:once)
+
+      expected_result = [
+        {
+          customer_id: 654321,
+          top_level_attribute: "an_attribute",
+          address_list: {
+            customer_address_attribute: "another_attribute",
+          }
+        }
+      ]
+
+       expect(ShopifyTransporter::MagentoCustomerExporter.new(1, soap_client).export).to eq(expected_result)
+    end
+  end
+end
---
+++ spec/shopify_transporter/exporter/magento/magento_order_exporter_spec.rb
@@ -0,0 +1,49 @@
+# frozen_string_literal: true
+require 'shopify_transporter/pipeline/stage'
+
+RSpec.describe ShopifyTransporter::MagentoOrderExporter do
+  context '#run' do
+    it 'works' do
+      soap_client = double("soap client")
+      sales_order_list_response_body = double('sales_order_list_response_body')
+      sales_order_info_response_body = double('sales_order_info_response_body')
+      expect(soap_client).to receive(:call).with(:sales_order_list, anything).and_return(sales_order_list_response_body).at_least(:once)
+      expect(sales_order_list_response_body).to receive(:body).and_return(
+        {
+          sales_order_list_response: {
+            result: {
+              item: [
+                {
+                  increment_id: 12345,
+                  top_level_attribute: "an_attribute",
+                }
+              ]
+            }
+          }
+        }
+      ).at_least(:once)
+      expect(soap_client).to receive(:call).with(:sales_order_info, order_increment_id: 12345).and_return(sales_order_info_response_body).at_least(:once)
+      expect(sales_order_info_response_body).to receive(:body).and_return(
+        {
+          sales_order_info_response: {
+            result: {
+              order_info_attribute: "another_attribute"
+            }
+          }
+        }
+      ).at_least(:once)
+
+      expected_result = [
+        {
+          increment_id: 12345,
+          top_level_attribute: "an_attribute",
+          items: {
+            order_info_attribute: "another_attribute",
+          }
+        }
+      ]
+
+       expect(ShopifyTransporter::MagentoOrderExporter.new(1, soap_client).export).to eq(expected_result)
+    end
+  end
+end

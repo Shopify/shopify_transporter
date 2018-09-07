1545d0667332dc0c3599819984f9f4d68937dbd9
Suraj Reddy <suraj.reddy@shopify.com>
2018-09-07 14:25:10 -0400

    Add a class to extract product mappings from magento

---
+++ lib/shopify_transporter/exporters/magento/product_mapping_exporter.rb
@@ -0,0 +1,64 @@
+# frozen_string_literal: true
+require 'sequel'
+
+module ShopifyTransporter
+  module Exporters
+    module Magento
+      class ProductMappingExporter
+        BATCH_SIZE = 1000
+
+        def initialize(
+          database: '',
+          host: '',
+          port: 3306,
+          user: '',
+          password: '',
+          filename: 'magento_product_mappings.csv'
+        )
+          @host = host
+          @port = port
+          @user = user
+          @password = password
+          @filename = filename
+          @database = database
+        end
+
+        def extract_mappings
+          write_headers
+
+          Sequel.connect(
+            adapter: :mysql2, user: @user, password: @password, host: @host, port: @port, database: @database
+          ) do |db|
+            ordered_mappings = db
+              .from(:catalog_product_relation)
+              .order(:parent_id)
+            current_id = ordered_mappings.first[:parent_id]
+            max_id = ordered_mappings.last[:parent_id]
+
+            while current_id < max_id
+              mappings_batch = ordered_mappings.where(parent_id: current_id...(current_id + BATCH_SIZE))
+              write_data(mappings_batch)
+              current_id += BATCH_SIZE
+            end
+          end
+        end
+
+        private
+
+        def write_headers
+          File.open(@filename, 'w') do |file|
+            file << "product_id,associated_product_id\n"
+          end
+        end
+
+        def write_data(mappings)
+          File.open(@filename, 'a') do |file|
+            mappings.each do |mapping|
+              file << "#{mapping[:parent_id]},#{mapping[:child_id]}\n"
+            end
+          end
+        end
+      end
+    end
+  end
+end
--- spec/shopify_transporter/exporters/magento/customer_exporter_spec.rb
+++ spec/shopify_transporter/exporters/magento/customer_exporter_spec.rb
@@ -1,5 +1,4 @@
 # frozen_string_literal: true
-require 'shopify_transporter/pipeline/stage'
 
 module ShopifyTransporter
   module Exporters

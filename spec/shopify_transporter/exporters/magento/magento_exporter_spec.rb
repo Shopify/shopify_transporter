42ed7bcd31920d849412a35359e87c4a9e18886a
Jordan Verasamy <j.verasamy@gmail.com>
2018-09-11 14:30:51 -0400

    remove useless module names

--- spec/shopify_transporter/exporters/magento/magento_exporter_spec.rb
+++ spec/shopify_transporter/exporters/magento/magento_exporter_spec.rb
@@ -4,28 +4,28 @@ require 'shopify_transporter/pipeline/stage'
 module ShopifyTransporter
   module Exporters
     module Magento
-      RSpec.describe ShopifyTransporter::Exporters::Magento::MagentoExporter do
+      RSpec.describe MagentoExporter do
         context '#for' do
           let(:fake_client) { double('fake_client') }
 
           it 'returns CustomerExporter for customer type' do
             expect(described_class.for(type: 'customer', store_id: 1, client: fake_client))
-              .to be_a(ShopifyTransporter::Exporters::Magento::CustomerExporter)
+              .to be_a(CustomerExporter)
           end
 
           it 'returns OrderExporter for order type' do
             expect(described_class.for(type: 'order', store_id: 1, client: fake_client))
-              .to be_a(ShopifyTransporter::Exporters::Magento::OrderExporter)
+              .to be_a(OrderExporter)
           end
 
           it 'returns ProductExporter for product type' do
             expect(described_class.for(type: 'product', store_id: 1, client: fake_client))
-              .to be_a(ShopifyTransporter::Exporters::Magento::ProductExporter)
+              .to be_a(ProductExporter)
           end
 
           it 'raises for any other type' do
             expect { described_class.for(type: 'nonexistent_type', store_id: 1, client: fake_client) }
-              .to raise_error(ShopifyTransporter::Exporters::Magento::MissingExporterError)
+              .to raise_error(MissingExporterError)
           end
         end
       end

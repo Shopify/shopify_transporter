43818e6cf0a7f8798aa230df6efc5dde843f6886
Jordan Verasamy <j.verasamy@gmail.com>
2018-09-11 09:49:24 -0400

    Add Magento 1.x exporter for basic product information  (#25)
    * initial commit
    
    * fix up
    
    * remove pry

---
+++ lib/shopify_transporter/exporters/magento/product_exporter.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+module ShopifyTransporter
+  module Exporters
+    module Magento
+      class ProductExporter
+        def initialize(store_id: nil, client: nil)
+          @client = client
+          @store_id = store_id
+        end
+
+        def export
+          $stderr.puts "Starting export..."
+          base_products.compact
+        end
+
+        private
+
+        def base_products
+          result = @client.call(:catalog_product_list, filters: nil).body
+          result[:catalog_product_list_response][:store_view][:item] || []
+        end
+      end
+    end
+  end
+end

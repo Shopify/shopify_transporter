069ff23366b99f830c9e6dab4ab3909b70e8c9ae
Ziyan Liu <ziyan.liu@shopify.com>
2018-09-14 14:57:46 -0400

    minor formatting changes, removed unnecessary comments

--- lib/shopify_transporter/pipeline/magento/order/line_items.rb
+++ lib/shopify_transporter/pipeline/magento/order/line_items.rb
@@ -42,12 +42,13 @@ module ShopifyTransporter
           end
 
           def tax_lines(item)
-            # Magento supports only one type of tax, the tax_lines should always be in array format
-            [{
-              title: 'Tax',
-              price: item['tax_amount'],
-              rate: item['tax_percent'],
-            }.stringify_keys]
+            [
+              {
+                title: 'Tax',
+                price: item['tax_amount'],
+                rate: item['tax_percent'],
+              }.stringify_keys,
+            ]
           end
         end
       end

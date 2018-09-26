a931c340f19ff6e352dde7fc7d7cd6fd62cd8fd3
Ziyan Liu <ziyan.liu@shopify.com>
2018-09-26 14:09:48 -0400

    remove debug statements

--- spec/shopify_transporter/pipeline/magento/product/top_level_variant_attributes_spec.rb
+++ spec/shopify_transporter/pipeline/magento/product/top_level_variant_attributes_spec.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 require 'shopify_transporter/pipeline/magento/product/top_level_variant_attributes'
-require 'pry'
+
 module ShopifyTransporter::Pipeline::Magento::Product
   RSpec.describe TopLevelVariantAttributes, type: :helper do
     context '#convert' do
@@ -57,7 +57,6 @@ module ShopifyTransporter::Pipeline::Magento::Product
       it 'should skip converting top level variants when the input is a product without parent_id' do
         simple_product_without_parent = FactoryBot.build(:simple_magento_product)
         simple_product_without_parent.delete("parent_id")
-        binding.pry
         variants_in_shopify_format = described_class.new.convert(simple_product_without_parent, {})
         expect(variants_in_shopify_format).to be_nil
       end

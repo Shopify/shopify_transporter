60a078ae588542b4db459addfc4d1e73e69eb565
Fahia Mohamed <fahia.mohamed@shopify.com>
2018-10-09 15:26:32 -0400

    More tests

--- spec/shopify_transporter/exporters/magento/soap_spec.rb
+++ spec/shopify_transporter/exporters/magento/soap_spec.rb
@@ -212,9 +212,20 @@ module ShopifyTransporter
               raise Savon::Error, 'Soap call failed.'
             end
 
-
-            expect { |b| soap_client.call_in_batches(method: :test_call, batch_index_column: 'customer_id').each(&b) }
-              .to yield_successive_args(['batch-0,1,2'], ['batch-6,7'])
+            stderr = nil
+            expect do |b|
+              stderr = capture(:stderr) { soap_client.call_in_batches(method: :test_call, batch_index_column: 'customer_id').each(&b) }
+            end.to yield_successive_args(['batch-0,1,2'], ['batch-6,7'])
+            output = <<~WARNING
+              Processing batch: 0..2
+              Processing batch: 3..5
+              Skipping batch: 3..5 after 4 retries because of an error.
+              The exact error was:
+              Savon::Error: 
+              Soap call failed.
+              Processing batch: 6..7
+            WARNING
+            expect(stderr).to eq(output)
           end
         end
       end

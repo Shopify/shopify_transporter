fe2a637f104d957c4baa131639b064d1b13407f8
Ziyan Liu <ziyan.liu@shopify.com>
2018-10-19 14:01:47 -0400

    remove debugging statement

--- lib/shopify_transporter/pipeline/magento/order/discounts.rb
+++ lib/shopify_transporter/pipeline/magento/order/discounts.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 require 'shopify_transporter/pipeline/stage'
 require 'shopify_transporter/shopify'
-require 'pry'
+
 module ShopifyTransporter
   module Pipeline
     module Magento

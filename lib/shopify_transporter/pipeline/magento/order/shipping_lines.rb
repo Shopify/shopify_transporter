3b6cca6542f941e183d393b84765ebd8ebb85839
Ziyan Liu <ziyan.liu@shopify.com>
2018-10-29 15:50:26 -0400

    switch back to use stringify_keys

--- lib/shopify_transporter/pipeline/magento/order/shipping_lines.rb
+++ lib/shopify_transporter/pipeline/magento/order/shipping_lines.rb
@@ -10,8 +10,8 @@ module ShopifyTransporter
           def convert(input, record)
             record.merge(
               {
-                'shipping_lines' => shipping_lines(input),
-              }
+                shipping_lines: shipping_lines(input),
+              }.stringify_keys
             )
           end
 
@@ -20,19 +20,19 @@ module ShopifyTransporter
           def shipping_lines(input)
             [
               {
-                'code' => input['shipping_description'],
-                'title' => input['shipping_description'],
-                'price'=> input['shipping_amount'],
-                'carrier_identifier'=> input['shipping_method'],
-                'tax_lines'=> shipping_tax_lines(input),
-              },
+                code: input['shipping_description'],
+                title: input['shipping_description'],
+                price: input['shipping_amount'],
+                carrier_identifier: input['shipping_method'],
+                tax_lines: shipping_tax_lines(input),
+              }.stringify_keys,
             ]
           end
 
           def shipping_tax_lines(input)
             return unless input['shipping_tax_amount'].present? && input['shipping_tax_amount'].to_f != 0
 
-            [{ 'price' => input['shipping_tax_amount'] }]
+            [{ price: input['shipping_tax_amount'] }.stringify_keys]
           end
         end
       end
